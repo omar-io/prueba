144512388bde79147296af3ecec94871d199f6b3
omar-ios <omar.ontiveros@alumnos.udg.mx>
2022-05-05 23:34:26 -0500

    first commit

---
+++ holamundo.cpp
@@ -0,0 +1,23 @@
+/* Omar Ontiveros Rios
+Practica 1
+Uso de  COUT */
+
+#include <iostream>
+#include <locale.h>
+#include <windows.h>
+using namespace std;
+
+main(){
+	setlocale(LC_ALL, "");
+	system("color F6");
+	cout << "\n\t     ´´´´´   " << endl;
+	cout << "\t     |||||     " << endl;
+	cout << "\t   @@@@@@@@@   " << endl;
+	cout << "\t   {~@~@~@~}   " << endl;
+	cout << "\t @@@@@@@@@@@@@ " << endl;
+	cout << "\t {~@ HAPPY @~} " << endl;
+	cout << "\t {  BIRTHDAY } " << endl;
+	cout << "\t @@@@@@@@@@@@@ " << endl;
+	cout << "\t    __) (__    " << endl;
+	cout << "\t   /_______\x5c\x61\x64\x6a" << endl;
+}
